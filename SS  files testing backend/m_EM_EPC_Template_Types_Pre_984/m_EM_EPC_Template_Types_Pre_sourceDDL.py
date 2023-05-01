7f4fe3f23a259a0c8153a5fcb591550f8a090708
srv-nucleusteq <117688191+srv-nucleusteq@users.noreply.github.com>
2023-05-01 03:16:39 -0700

    SS  files testing backend project is uploaded on github

---
+++ SS  files testing backend/m_EM_EPC_Template_Types_Pre_984/m_EM_EPC_Template_Types_Pre_sourceDDL.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS DELTA_TRAINING;
+
+CREATE TABLE IF NOT EXISTS DELTA_TRAINING.EPC_TemplateTypes(TemplateTypeId INT,
+TemplateTypeSAPId STRING,
+TemplateTypeName STRING,
+TemplateTypeDescription STRING) USING DELTA;
---
+++ SS  files testing backend/m_EM_EPC_Template_Types_Pre_984/mappings/m_EM_EPC_Template_Types_Pre.py
@@ -0,0 +1,54 @@
+# Databricks notebook source
+# MAGIC %run "./udf_informatica"
+
+# COMMAND ----------
+
+
+from pyspark.sql.types import *
+
+spark.sql("use DELTA_TRAINING")
+spark.sql("set spark.sql.legacy.timeParserPolicy = LEGACY")
+
+
+# COMMAND ----------
+# DBTITLE 1, Shortcut_to_EPC_TemplateTypes_0
+
+
+df_0 = spark.sql("""SELECT
+  TemplateTypeId AS TemplateTypeId,
+  TemplateTypeSAPId AS TemplateTypeSAPId,
+  TemplateTypeName AS TemplateTypeName,
+  TemplateTypeDescription AS TemplateTypeDescription
+FROM
+  EPC_TemplateTypes""")
+
+df_0.createOrReplaceTempView("Shortcut_to_EPC_TemplateTypes_0")
+
+# COMMAND ----------
+# DBTITLE 1, SQ_Shortcut_to_EPC_TemplateTypes_1
+
+
+df_1 = spark.sql("""SELECT
+  TemplateTypeId AS TemplateTypeId,
+  TemplateTypeSAPId AS TemplateTypeSAPId,
+  TemplateTypeName AS TemplateTypeName,
+  TemplateTypeDescription AS TemplateTypeDescription,
+  monotonically_increasing_id() AS Monotonically_Increasing_Id
+FROM
+  Shortcut_to_EPC_TemplateTypes_0""")
+
+df_1.createOrReplaceTempView("SQ_Shortcut_to_EPC_TemplateTypes_1")
+
+# COMMAND ----------
+# DBTITLE 1, EM_EPC_TEMPLATE_TYPES_PRE
+
+
+spark.sql("""INSERT INTO
+  EM_EPC_TEMPLATE_TYPES_PRE
+SELECT
+  TemplateTypeId AS EM_AMS_TEMPLATE_TYPE_ID,
+  TemplateTypeSAPId AS EM_SAP_TEMPLATE_TYPE_ID,
+  TemplateTypeName AS EM_AMS_TEMPLATE_TYPE_NAME,
+  TemplateTypeDescription AS EM_AMS_TEMPLATE_TYPE_DESC
+FROM
+  SQ_Shortcut_to_EPC_TemplateTypes_1""")
